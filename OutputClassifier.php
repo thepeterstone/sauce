892ebbc5f28e51709b83582740b790e7d53c0b67
Peter Stone <thepeterstone@gmail.com>
2012-01-31 00:27:41 -0800

    first test

--- OutputClassifier.php
+++ OutputClassifier.php
@@ -1,5 +1,15 @@
 <?php
 
 class OutputClassifier {
+	private $classifiers = array(
+		'/-bash: (\w+): command not found/',
+	);
 
+
+	public function check($string) {
+		foreach ($this->classifiers as $test) {
+			if (preg_match($test, $string)) return true;
+		}
+		return false;
+	}
 }
--- OutputClassifierTest.php
+++ OutputClassifierTest.php
@@ -1,6 +1,12 @@
 <?php
 
+require_once 'OutputClassifier.php';
 require_once 'PHPUnit/Autoload.php';
 
 class OutputClassifierTest extends PHPUnit_Framework_TestCase {
+	public function testBashCommandNotFoundIsRecognized( )
+	{
+		$oc = new OutputClassifier();
+		$this->assertTrue($oc->check("-bash: fark: command not found"));
+	}
 }
